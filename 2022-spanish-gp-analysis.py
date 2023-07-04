b7fbc2b83e6a4580c142e67e5891dbaa070d00a8
Orabi <36137101+ORABI2@users.noreply.github.com>
2023-07-04 05:28:33 +0300

    Update 2022-spanish-gp-analysis.py

--- 2022-spanish-gp-analysis.py
+++ 2022-spanish-gp-analysis.py
@@ -8,16 +8,22 @@ import fastf1.plotting
 fastf1.plotting.setup_mpl(misc_mpl_mods=False)
 # load a session and its telemetry data
 session = fastf1.get_session(2022, 'Spanish Grand Prix', 'R')
+# Loading the session 
 session.load()
+# getting the fastes lap for max verstappen
 ver_lap = session.laps.pick_driver('VER').pick_fastest()
+# getting the fastes lap for lewis hamilton
 ham_lap = session.laps.pick_driver('HAM').pick_fastest()
 
+# getting the ditance
 ver_tel = ver_lap.get_car_data().add_distance()
 ham_tel = ham_lap.get_car_data().add_distance()
 
+# getting the color themes for each team to assign to the driver
 rbr_color = fastf1.plotting.team_color('RBR')
 mer_color = fastf1.plotting.team_color('MER')
 
+# visualizing...
 fig, ax = plt.subplots()
 ax.plot(ver_tel['Distance'], ver_tel['Speed'], color = rbr_color, label = 'VERSTAPPEN')
 ax.plot(ham_tel['Distance'], ham_tel['Speed'], color = mer_color, label = 'HAMILTON')
